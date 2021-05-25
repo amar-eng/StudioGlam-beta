b77f32ea44b633c19663130721b99a23dbadd510
amar-eng <am24mo@gmail.com>
2021-05-24 22:41:36 -0400

    dotenv added; see if it comes up on github

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
 const cors = require('cors');
+
+dotenv.config()
+
 const app = express()
 
 
@@ -17,5 +21,6 @@ app.get('/api/products/:id', (req,res) =>{
     const product = products.find(p => p._id === req.params.id)
     res.json(product)
 })
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log('Server running on PORT 5000'))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on  port ${PORT}`))
