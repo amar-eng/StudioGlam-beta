fe8e7833c61a54dc7ede5768f0ed47d52b94e3fd
amar-eng <am24mo@gmail.com>
2021-06-03 18:05:04 -0400

    User login screen and functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import {BrowserRouter as Router, Route  } from 'react-router-dom' 
-import { Container } from 'react-bootstrap'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen'
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
     <Router>
-      <Header/>
-      <main className='py-3'>
+      <Header />
+      <main className="py-3">
         <Container>
-          <Route exact path ='/' component ={HomeScreen}/> 
-          <Route  path ='/product/:id' component ={ProductScreen}/> 
-          <Route  path ='/cart/:id?' component ={CartScreen}/> 
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </Router>
   );
-}
+};
 
 export default App;
