1fb58fd82f1572751c0b3d4127a327daa468dfbd
amar-eng <am24mo@gmail.com>
2021-07-31 17:37:30 -0400

    user update completed

--- client/src/screens/UserEditScreen.js
+++ client/src/screens/UserEditScreen.js
@@ -6,7 +6,7 @@ import Mesage from "../components/Mesage";
 import Loader from "../components/Loader";
 import FormContainer from "../components/FormContainer";
 import { getUserDetails, updateUser } from "../actions/userActions";
-
+import { USER_UPDATE_RESET } from "../constants/userConstants";
 const UserEditScreen = ({ match, history }) => {
   const userId = match.params.id;
   const [name, setName] = useState("");
@@ -16,18 +16,32 @@ const UserEditScreen = ({ match, history }) => {
 
   const userDetails = useSelector((state) => state.userDetails);
   const { loading, error, user } = userDetails;
+
+  const userUpdate = useSelector((state) => state.userUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = userUpdate;
+
   useEffect(() => {
-    if (!user.name || user._id !== userId) {
-      dispatch(getUserDetails(userId));
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET });
+      history.push("/admin/userlist");
     } else {
-      setName(user.name);
-      setEmail(user.email);
-      setIsAdmin(user.isAdmin);
+      if (!user.name || user._id !== userId) {
+        dispatch(getUserDetails(userId));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+        setIsAdmin(user.isAdmin);
+      }
     }
-  }, [dispatch, userId, user]);
+  }, [dispatch, userId, user, successUpdate, history]);
 
   const submitHandler = (e) => {
     e.preventDefault();
+    dispatch(updateUser({ _id: userId, name, email, isAdmin }));
   };
 
   return (
@@ -37,8 +51,8 @@ const UserEditScreen = ({ match, history }) => {
       </Link>
       <FormContainer>
         <h1>Edit User</h1>
-        {/* {loading && <Loader />}
-        {error && <Mesage variant="danger">{error}</Mesage>} */}
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Mesage variant="danger">{errorUpdate}</Mesage>}
         {loading ? (
           <Loader />
         ) : error ? (
