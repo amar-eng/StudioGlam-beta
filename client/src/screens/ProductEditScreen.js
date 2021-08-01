34cb6443edc76ed1e2b63f95e36f9bc3179cbad8
amar-eng <am24mo@gmail.com>
2021-08-01 14:35:41 -0400

    image uploader using multet created

--- client/src/screens/ProductEditScreen.js
+++ client/src/screens/ProductEditScreen.js
@@ -51,6 +51,29 @@ const ProductEditScreen = ({ match, history }) => {
     }
   }, [dispatch, history, productId, product, successUpdate]);
 
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      const { data } = await axios.post("/api/upload", formData, config);
+
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      console.error(error);
+      setUploading(false);
+    }
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
     dispatch(
@@ -109,13 +132,13 @@ const ProductEditScreen = ({ match, history }) => {
                 value={image}
                 onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
-              {/* <Form.File
+              <Form.File
                 id="image-file"
-                label="Choose File"
                 custom
-                onChange={(uploadFileHandler)}
-              ></Form.File> */}
-              {/* {uploading && <Loader />} */}
+                onChange={uploadFileHandler}
+                style={{ marginTop: "1rem", marginBottom: "1rem" }}
+              ></Form.File>
+              {uploading && <Loader />}
             </Form.Group>
 
             <Form.Group controlId="brand">
