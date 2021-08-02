18ef644f38c4b53178762b7afeb873e447e4ae67
amar-eng <am24mo@gmail.com>
2021-08-02 13:56:20 -0400

    mark as delivered functionality added

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -16,6 +16,8 @@ import {
   ORDER_LIST_SUCCESS,
   ORDER_LIST_REQUEST,
   ORDER_DELIVER_FAIL,
+  ORDER_DELIVER_SUCCESS,
+  ORDER_DELIVER_REQUEST,
 } from "../constants/orderConstants";
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -123,6 +125,47 @@ export const payOrder =
       });
     }
   };
+
+export const deliverOrder = (order) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_DELIVER_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      `/api/orders/${order._id}/deliver`,
+      {},
+      config
+    );
+
+    dispatch({
+      type: ORDER_DELIVER_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    // if (message === "Not authorized, token failed") {
+    //   dispatch(logout());
+    // }
+    dispatch({
+      type: ORDER_DELIVER_FAIL,
+      payload: message,
+    });
+  }
+};
 
 export const listMyOrders = () => async (dispatch, getState) => {
   try {
